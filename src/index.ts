500f882e99139415b811a6d55ce4a01297488252
robinovitch61 <leorobinovitch@gmail.com>
2018-08-04 14:33:55 -0700

    Show xkcd command on panel

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,29 @@ const extension: JupyterLabPlugin<void> = {
   requires: [ICommandPalette],
   activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_xkcd is activated!');
-    console.log('ICommandPalette:', palette);
+
+    // Create a single widget
+    let widget: Widget = new Widget();
+    widget.id = 'xkcd-jupyterlab';
+    widget.title.label = 'xkcd.com';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Random xkcd comic',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
 
